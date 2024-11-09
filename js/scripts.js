494260c52834efa7a737fc8d683fb93531614878
Kaly67 <richertfleur@gmail.com>
2024-11-09 14:10:55 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,31 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+document.getElementById('contactForm').addEventListener('input', function() {
+    const form = this;
+    const isValid = form.checkValidity();  // Vérifie la validité de tous les champs
+    const submitButton = document.getElementById('submitButton');
+    if (isValid) {
+        submitButton.classList.remove('disabled');
+        submitButton.removeAttribute('disabled');
+    } else {
+        submitButton.classList.add('disabled');
+        submitButton.setAttribute('disabled', 'true');
+    }
+});
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Empêche l'envoi réel du formulaire
+
+    const form = this;
+    const isValid = form.checkValidity();
+
+    if (isValid) {
+        // Simulation de succès de la soumission
+        document.getElementById('submitSuccessMessage').classList.remove('d-none');
+        document.getElementById('submitErrorMessage').classList.add('d-none');
+    } else {
+        // Simulation d'échec de la soumission
+        document.getElementById('submitErrorMessage').classList.remove('d-none');
+        document.getElementById('submitSuccessMessage').classList.add('d-none');
+    }
+});
